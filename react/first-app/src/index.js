e1b0a6e15bfdcc912a229fc42672b3a866639c3b
nissi153 <nissisoft21@gmail.com>
2024-08-26 13:58:41 +0900

    Update index.js

--- react/first-app/src/index.js
+++ react/first-app/src/index.js
@@ -6,4 +6,7 @@ import ReactDOM from "react-dom/client"; //React.js client Next.js/server
 import Clock from "./4-element/Clock";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<App />);
+//root.render(<Book name="리액트기초" price="1000" />);
+//root.render(<VM price="1000" />);
 root.render(<Clock />);
