81eadf63e99a4538abd415420478fac063aa3237
nissi153 <nissisoft21@gmail.com>
2024-08-27 08:53:04 +0900

    update

--- react/first-app/src/index.js
+++ react/first-app/src/index.js
@@ -12,10 +12,11 @@ import ReactDOM from "react-dom/client"; //React.js client Next.js/server
 //import Accommodate from "./7-hook/Accommodate";
 // import ConfirmButton from "./8-event/ConfirmButton2";
 //import LandingPage from "./9-conditional-render/LandingPage";
-import AttendanceBook from "./10-list/AttendanceBook";
+// import AttendanceBook from "./10-list/AttendanceBook";
+import NameForm from "./11-form/NameForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //root.render(<App />);
 //root.render(<Book name="리액트기초" price="1000" />);
 //root.render(<VM price="1000" />);
-root.render(<AttendanceBook />);
+root.render(<NameForm />);
