5c5cf1de3ee3f1b83becbec31cab1364cbdbcd2d
nissi153 <nissisoft21@gmail.com>
2024-08-26 13:56:25 +0900

    Update index.js

--- react/first-app/src/index.js
+++ react/first-app/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; //React.js client Next.js/server
-import App from "./App";
-import Book from "./3-jsx/Book";
-import VM from "./3-jsx/VendingMachine";
+// import App from "./App";
+// import Book from "./3-jsx/Book";
+// import VM from "./3-jsx/VendingMachine";
+import Clock from "./4-element/Clock";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<VM price="1000" />);
+root.render(<Clock />);
