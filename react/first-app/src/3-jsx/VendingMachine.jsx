c473ead738ffc24896085661b7d857ed23a23d8d
nissi153 <nissisoft21@gmail.com>
2024-08-26 13:39:55 +0900

    update

--- react/first-app/src/3-jsx/VendingMachine.jsx
+++ react/first-app/src/3-jsx/VendingMachine.jsx
@@ -1,5 +1,25 @@
 // 연습문제
-// props로 1000원을 받으면,
+// props로
+// 1000원을 받으면 : 콜라
 // 2000원 : 사이다
 // 3000원 : 이온음료
-// 받은 금액과 상품이름-콜라을 UI에 출력하시오.
+// 받은 금액과 상품이름을 UI에 출력하시오.
+
+import React from "react";
+
+function VM(props) {
+  return (
+    <>
+      <h1>{`받은 금액은 ${props.price}원 입니다.`}</h1>
+      <h1>
+        {props.price == 1000
+          ? "콜라"
+          : props.price == 2000
+          ? "사이다"
+          : "이온음료"}
+      </h1>
+    </>
+  );
+}
+
+export default VM;
--- react/first-app/src/index.js
+++ react/first-app/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"; //React.js client Next.js/server
 import App from "./App";
 import Book from "./3-jsx/Book";
+import VM from "./3-jsx/VendingMachine";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Book name="리액트 초급" price="1000" />);
+root.render(<VM price="1000" />);
