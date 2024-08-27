c8976a1219a16ac2ad4545467c10823640e6ea10
nissi153 <nissisoft21@gmail.com>
2024-08-27 15:55:46 +0900

    update

--- react/mini-blog/src/App.js
+++ react/mini-blog/src/App.js
@@ -1,7 +1,19 @@
-// import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+
+//Pages
+// MainPage
+// PostWritePage
+// PostViewPage
+
+const MainTitleText = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
 
 function App() {
-  return <div className="App">Welcom React.</div>;
+  return <MainTitleText>홍길동의 미니블로그</MainTitleText>;
 }
 
 export default App;
