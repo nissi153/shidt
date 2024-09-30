00c687264b7f3df85d82ed0f48e7c5d6051ab601
nissi153 <nissisoft21@gmail.com>
2024-09-30 11:28:29 +0900

    update

--- csharp/Examples/ex02.cs
+++ csharp/Examples/ex02.cs
@@ -43,15 +43,42 @@ namespace Examples
             Console.WriteLine(myStr1);
             Console.WriteLine(myStr1.GetType().Name);
 
-            //연습문제
+            //변수(함수) 이름 짓는 법
+            //1. 영소문자로 시작
+            //2. Camel Case(낙타등)을 사용(단어의 시작은 대문자로)
+            //  예) myStudentCount
+            //3. _(언더바) 외에 특수문자(!@#$) 사용 불가
+            //4. 상수는 대문자로
+            //5. 클래스 이름은 첫글자가 대문자
+
+            //연습문제-1
             //1. 정수형 변수 100을 선언하고,
             //  실수형으로 형변환 한 후에 그 변수를 출력하시오.
+            int myNumber = 100;
+            float myFloatNumber = myNumber; //묵시적(암시적) 강제
+            Console.WriteLine(myFloatNumber);
+            Console.WriteLine(myFloatNumber.GetType().Name); //Single
+
             //2. 실수형 변수 3.14를 선언하고
             // 정수형으로 형변환 한 후에 그 변수를 출력하시오.
+            double myDoubleNumber = 3.14;
+            Console.WriteLine(myDoubleNumber.GetType().Name); //Double
+            int myNumber2 = (int)myDoubleNumber;
+            Console.WriteLine(myNumber2);
+            Console.WriteLine(myNumber2.GetType().Name);
+
             //3. int 타입 변수에 200을 넣어 선언하고
             //  long타입으로 형변환 한 후에 출력하시오.
+            int myNumber3 = 200;
+            long myLongNumber = myNumber3;
+            Console.WriteLine(myLongNumber);
+            Console.WriteLine(myLongNumber.GetType().Name);
+
             //4. long 타입 변수에 200억을 넣어 선언하고,
             // int 타입으로 형변환 한 후에 출력하시오.(잘림현상 발생!)
+            long myLongNumber2 = 20_000_000_000L;
+            int myNumber4 = (int)myLongNumber2;
+            Console.WriteLine(myNumber4);
 
         }
     }
