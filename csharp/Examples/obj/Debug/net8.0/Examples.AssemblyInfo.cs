2681c91f353e0fc206c75f5bcc1250e35987292e
nissi153 <nissisoft21@gmail.com>
2024-09-30 10:56:35 +0900

    update

--- csharp/Examples/obj/Debug/net8.0/Examples.AssemblyInfo.cs
+++ csharp/Examples/obj/Debug/net8.0/Examples.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce0a44a5e3d1e17cca70006747349ae250ce2b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Examples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
