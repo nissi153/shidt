be5f213744f6f147fc5c1288853302bc62dddab4
nissi153 <nissisoft21@gmail.com>
2024-10-02 09:19:01 +0900

    up

---
+++ csharp/Examples/ex09.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Examples {
+    internal class ex09 {
+        static void Main() {
+
+        }
+        //연습문제 7
+        //1. 원주율 구하는 함수를 선언하고,
+        //  매개변수로 반지름을 입력하면 원주율을 반환하여 출력하시오.
+        //패턴
+        //static double 함수이름( 반지름 ) {
+        //  return 원주율;
+        //}
+        //2. 정수 3개를 매개변수를 입력받고, 그중 가장 큰 수를 반환하는 함수를
+        // 선언하시오. 
+        // 입력예) 10,20,30 매개변수로 입력
+        // 출력예) 30
+    }
+}
