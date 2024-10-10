2fde88366f0af07894573ce6cda26644579022bf
nissi153 <nissisoft21@gmail.com>
2024-10-10 14:06:47 +0900

    update

--- csharp/Examples/ex14.cs
+++ csharp/Examples/ex14.cs
@@ -26,6 +26,40 @@ namespace Examples {
             //3. 순회 종료 후에는 max는 최대값이 된다.
             Console.WriteLine(max);
             Console.WriteLine(min);
+
+            //정렬(Sort) 알고리즘
+            // 1. 버블 정렬
+            // 2. 선택 정렬
+            // 3. 삽입 정렬
+
+            //Swap 치환
+            int a = 10;
+            int b = 20;
+            //a에 b값을, b에 a값을 넣고 싶다.
+            //a = b;//20 = 20
+            //b = a;//20 = 20
+            //치환용 임시변수를 사용한다.
+            int temp = 0;
+            temp = a;
+            a = b;
+            b = temp;
+
+            //1.버블정렬 : 오름차순으로
+            nums = [10, 30, 20, 50, 40];
+            for(int i = 0; i < 5; i++) {
+                for(int j = i+1; j<5; j++) {
+                    if( nums[i] > nums[j]) {
+                        //치환!
+                        int tmp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = tmp;
+                    }
+                }
+            }
+            foreach(int num in nums) {
+                Console.WriteLine(num);
+            }
+
         }
     }
 }
--- csharp/Examples/obj/Debug/net8.0/Examples.AssemblyInfo.cs
+++ csharp/Examples/obj/Debug/net8.0/Examples.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae72c768bf9a58218e730772d740ec3942842a5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8735cc81b039955d93a6d7b8f3a3431f42c75895")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Examples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
